d7a3499399a370fad8c566d4fb6fb571a8d17049
alscho <alexander2791@hotmail.com>
2025-01-13 13:37:47 +0100

    added upload thumbnail functionality

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 )
 
@@ -28,10 +32,83 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	// parse to multipart and get data + metadata
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't parse data", err)
+		return
+	}
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse from file", err)
+		return
+	}
+	defer file.Close()
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	// analyze metadata to get image type, if possible
+	contentType := header.Header.Get("Content-Type")
+	if contentType == "" {
+		respondWithError(w, http.StatusBadRequest, "Couldn't find valid Content-Type header", nil)
+		return
+	}
+	parts := strings.Split(contentType, "/")
+	if len(parts) != 2 {
+		respondWithError(w, http.StatusBadRequest, "malformed Content-Type header", nil)
+		return
+	}
+	mainType := parts[0]
+	// subType := parts[1]
+	if mainType != "image" {
+		respondWithError(w, http.StatusBadRequest, "Content Type doesn't match image data type - thumbnail has to be an image", nil)
+		return
+	}
+
+	// get image data
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't read image data", err)
+		return
+	}
+
+	// fetch video meta data, if possible
+	metaData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't find video with such id", err)
+		return
+	}
+	if metaData.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "access permitted", nil)
+		return
+	}
+
+	// save thumbnail to global map
+	videoThumbnail := thumbnail{
+		data:      imageData,
+		mediaType: contentType,
+	}
+	videoThumbnails[videoID] = videoThumbnail
+
+	// update database thumbnail_url of video at videoID
+	thumbnailUrl := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoIDString)
+	updatedVideo := database.Video{
+		ID:           metaData.ID,
+		CreatedAt:    metaData.CreatedAt,
+		UpdatedAt:    time.Now(),
+		ThumbnailURL: &thumbnailUrl,
+		VideoURL:     metaData.VideoURL,
+		CreateVideoParams: database.CreateVideoParams{
+			Title:       metaData.Title,
+			Description: metaData.Description,
+			UserID:      metaData.UserID,
+		},
+	}
+	err = cfg.db.UpdateVideo(updatedVideo)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video, although videoID had been found", err)
+	}
+
+	respondWithJSON(w, http.StatusOK, updatedVideo)
 }
